ec1698e1f716e6799cf739d91ef2ee165d248c6a
igirishr1 <girishreddyk7@gmail.com>
2023-05-27 12:35:46 +0530

    Update Number_Guess.py

--- Number_Guess.py
+++ Number_Guess.py
@@ -1,5 +1,15 @@
 import random 
-t = int(input("Enter the max value of number"))
-r = random.randint(t)
-print(r)
+g = random.randrange(1,100)
+u = 0
 
+while u != g:
+    u = int(input("Enter a number: "))
+
+    if u > g:
+        print("Your num is greater than generated number.")
+
+    elif u < g:
+        print("Your num is less than generated number.")
+
+    elif u == g:
+        print("Correct!")
